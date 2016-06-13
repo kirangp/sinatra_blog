d607bb7c13ad57ee3730a6637d93b10b947f693f
phaltoe <reverse9@live.com>
2016-06-13 09:53:33 -0300

    migrations and models created

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,16 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    set :session_secret, "authentication_sinatra"
+  end
+
+  get "/" do
+    erb :welcome
+  end
+
+end
